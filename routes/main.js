f6439b9c20753c9fb2d28c53409190b217e9a3f1
pedro554 <63167281+pedro554@users.noreply.github.com>
2021-10-03 19:45:10 -0300

    0.3

--- routes/main.js
+++ routes/main.js
@@ -1,14 +1,33 @@
 const express = require('express')
 const router = express.Router()
+const controllers =  require('../controllers')
 
-router.get('/', (req, res) => {
-
-    res.render('home', req.context)
+router.get('/', async (req, res) => {
+    const data = req.context
+    const itemCtr = controllers.item.instance()
+    data.cafe = await itemCtr.get({category: 'cafe'})
+    data.cha = await itemCtr.get({category: 'cha'})
+    data.pastries = await itemCtr.get({category: 'pastries'})
+    res.render('home', data)
 })
 
 router.get('/blog', (req, res) => {
 
     res.render('blog', req.context)
+})
+
+router.get('/items', async (req, res) => {
+    const filters = req.query
+    const itemCtr = controllers.item.instance()
+    const items = await itemCtr.get(filters)
+    res.json({ items })
+})
+
+router.post('/order', async (req, res) => {
+    const orderData = req.body
+    const orderCtr = controllers.order.instance()
+    const order = await orderCtr.post(orderData)
+    res.json(order)
 })
 
 module.exports = router
